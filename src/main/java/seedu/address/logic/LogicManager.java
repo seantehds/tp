9e3306dd7d0dbfddb0d06149453f7096daf3b828
seantehds <seanteh2001@gmail.com>
2023-09-28 11:54:58 +0800

    Add remark command to app

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -47,10 +47,14 @@ public class LogicManager implements Logic {
         logger.info("----------------[USER COMMAND][" + commandText + "]");
 
         CommandResult commandResult;
+        //Parse user input from String to a Command
         Command command = addressBookParser.parseCommand(commandText);
+        //Executes the Command and stores the result
         commandResult = command.execute(model);
 
         try {
+            // We can deduce that the previous line of code modifies model in some way
+            // since it's being stored here.
             storage.saveAddressBook(model.getAddressBook());
         } catch (AccessDeniedException e) {
             throw new CommandException(String.format(FILE_OPS_PERMISSION_ERROR_FORMAT, e.getMessage()), e);
---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Remark;
+import seedu.address.testutil.PersonBuilder;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+class RemarkCommandTest {
+    private static final String REMARK_STUB = "RemarkStub";
+
+    @Test
+    void execute_addRemarkUnfilteredList_success(Model model) {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(firstPerson).withRemark(REMARK_STUB).build();
+
+        RemarkCommand remarkCommand = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(editedPerson.getRemark().value));
+
+        String expectedMessage = String.format(RemarkCommand.MESSAGE_ADD_REMARK_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(remarkCommand, model, expectedMessage, expectedModel);
+    }
+}
