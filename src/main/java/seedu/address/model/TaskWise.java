9bacdbdd15d1f1812a30491fe68dc9eda23e458f
JCSnap <e0970167@u.nus.edu>
2023-10-10 21:34:48 +0800

    Add more placeholder files

---
+++ src/main/java/seedu/address/model/TaskWise.java
@@ -0,0 +1,35 @@
+package seedu.address.model;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.task.Task;
+import seedu.address.model.task.UniqueTaskList;
+
+public class TaskWise implements ReadOnlyTaskWise {
+    @Override
+    public String toString() {
+        // TODO: implement
+        return null;
+    }
+    @Override
+    public ObservableList<Task> getTaskList() {
+        // TODO: implement
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // TODO: implement
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO: implement
+        return 0;
+    }
+}
--- src/main/java/seedu/address/model/task/UniqueTaskList.java
+++ src/main/java/seedu/address/model/task/UniqueTaskList.java
@@ -5,21 +5,25 @@ import java.util.Iterator;
 public class UniqueTaskList implements Iterable<Task> {
     @Override
     public Iterator<Task> iterator() {
+        // TODO: implement
         return null;
     }
 
     @Override
     public boolean equals(Object other) {
+        // TODO: implement
         return true;
     }
 
     @Override
     public int hashCode() {
+        // TODO: implement
         return 0;
     }
 
     @Override
     public String toString() {
+        // TODO: implement
         return "";
     }
 }
