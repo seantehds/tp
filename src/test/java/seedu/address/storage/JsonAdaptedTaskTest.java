4d7b9df1529a03c649688e4a48dc2060616d48de
JCSnap <e0970167@u.nus.edu>
2023-10-24 12:47:33 +0800

    Add test for invalid note in JsonAdaptedTaskTest

--- src/test/java/seedu/address/storage/JsonAdaptedTaskTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedTaskTest.java
@@ -12,13 +12,16 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.task.Description;
+import seedu.address.model.task.Note;
 import seedu.address.storage.exceptions.json.IllegalJsonValueException;
 
 public class JsonAdaptedTaskTest {
-    private static final String INVALID_NAME = "R@chel";
+    private static final String INVALID_DESCRIPTION = "Do OP2 @ Slides";
+    private static final String INVALID_NOTE = "clarify ^&with tutor";
     private static final String INVALID_TAG = "#friend";
 
-    private static final String VALID_NAME = BENSON.getDescription().toString();
+    private static final String VALID_DESCRIPTION = BENSON.getDescription().toString();
+    private static final String VALID_NOTE = BENSON.getNote().toString();
     private static final List<JsonAdaptedTag> VALID_TAGS = BENSON.getTags().stream()
             .map(JsonAdaptedTag::new)
             .collect(Collectors.toList());
@@ -32,14 +35,14 @@ public class JsonAdaptedTaskTest {
     @Test
     public void toModelType_invalidName_throwsIllegalValueException() {
         JsonAdaptedTask task =
-                new JsonAdaptedTask(INVALID_NAME, VALID_TAGS, false);
+                new JsonAdaptedTask(INVALID_DESCRIPTION, VALID_TAGS, false, VALID_NOTE);
         String expectedMessage = Description.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalJsonValueException.class, expectedMessage, task::toModelType);
     }
 
     @Test
     public void toModelType_nullName_throwsIllegalValueException() {
-        JsonAdaptedTask task = new JsonAdaptedTask(null, VALID_TAGS, false);
+        JsonAdaptedTask task = new JsonAdaptedTask(null, VALID_TAGS, false, VALID_NOTE);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Description.class.getSimpleName());
         assertThrows(IllegalJsonValueException.class, expectedMessage, task::toModelType);
     }
@@ -48,8 +51,16 @@ public class JsonAdaptedTaskTest {
     public void toModelType_invalidTags_throwsIllegalValueException() {
         List<JsonAdaptedTag> invalidTags = new ArrayList<>(VALID_TAGS);
         invalidTags.add(new JsonAdaptedTag(INVALID_TAG));
-        JsonAdaptedTask task = new JsonAdaptedTask(VALID_NAME, invalidTags, false);
+        JsonAdaptedTask task = new JsonAdaptedTask(VALID_DESCRIPTION, invalidTags, false, VALID_NOTE);
         assertThrows(IllegalJsonValueException.class, task::toModelType);
     }
 
+    @Test
+    public void toModelType_invalidNote_throwsIllegalValueException() {
+        JsonAdaptedTask task =
+                new JsonAdaptedTask(VALID_DESCRIPTION, VALID_TAGS, false, INVALID_NOTE);
+        String expectedMessage = Note.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalJsonValueException.class, expectedMessage, task::toModelType);
+    }
 }
+
