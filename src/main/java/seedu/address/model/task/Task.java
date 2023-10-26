9e87b1d025959c89e30406659f10930ea09da249
JCSnap <e0970167@u.nus.edu>
2023-10-26 17:56:06 +0800

    Add note and deadline to hashcode

--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -116,7 +116,7 @@ public class Task {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(description, tags, status);
+        return Objects.hash(description, tags, status, note, deadline);
     }
 
     @Override
