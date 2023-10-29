6ca60a19adb4d77bad9be89f667115d0e74740e0
George Tay <george_tay@u.nus.edu>
2023-10-29 21:09:54 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/ui/TaskListPanel.java
+++ src/main/java/seedu/address/ui/TaskListPanel.java
@@ -2,30 +2,18 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.SelectionMode;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Paint;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.task.Priority;
 import seedu.address.model.task.Task;
@@ -53,16 +41,14 @@ public class TaskListPanel extends UiPart<Region> {
         super(FXML);
         taskListView.setItems(taskList);
         taskListView.setCellFactory(listView -> new TaskListViewCell());
-        taskListView.getSelectionModel().selectedItemProperty().addListener(
-                (observable, oldValue, newValue) -> {
-                    if (newValue != null && !newValue.isSameTask(oldValue)) {
-                        this.setTaskInformation(newValue);
+        taskListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null && !newValue.isSameTask(oldValue)) {
+                this.setTaskInformation(newValue);
 
-                        // scroll to avoid jumpy behaviour
-                        this.taskListView.getSelectionModel().select(newValue);
-                    }
-                }
-        );
+                // scroll to avoid jumpy behaviour
+                this.taskListView.getSelectionModel().select(newValue);
+            }
+        });
 
         //@@author asdfgjkxd-reused
         // reused from https://copyprogramming.com/howto/implementing-a-global-key-press-for-javafx-methods
@@ -78,29 +64,17 @@ public class TaskListPanel extends UiPart<Region> {
         );
         //@@author
 
-        taskInformationView.setVisible(false);
+        this.destroyTaskInformationView();
     }
 
     public void setTaskInformation(Task task) {
-        BackgroundFill backgroundFill = new BackgroundFill(
-                Paint.valueOf("#FCF6DB"), new CornerRadii(10.0), Insets.EMPTY
-        );
-
         // show side panel
-        this.gridPane.getColumnConstraints().get(0).setPercentWidth(60.0);
-        this.gridPane.getColumnConstraints().get(1).setPercentWidth(40.0);
+        // this.gridPane.getColumnConstraints().get(0).setPercentWidth(60.0);
+        // this.gridPane.getColumnConstraints().get(1).setPercentWidth(40.0);
 
         // set up the view
-        this.taskInformationView.setVisible(true);
         this.taskInformationView.getChildren().clear();
-        this.taskInformationView.setBackground(
-                new Background(backgroundFill)
-        );
-        this.taskInformationView.setPadding(
-                new Insets(5, 5, 5, 5)
-        );
         this.taskInformationView.setAlignment(Pos.TOP_LEFT);
-
 
         Label descriptionView = this.getDescriptionField(task);
         HBox statusView = this.getStatusField(task);
@@ -116,11 +90,25 @@ public class TaskListPanel extends UiPart<Region> {
 
     private void destroyTaskInformationView() {
         // set up the view
-        this.taskInformationView.setVisible(false);
+        // this.taskInformationView.setVisible(false);
 
         // show side panel
-        this.gridPane.getColumnConstraints().get(0).setPercentWidth(100.0);
-        this.gridPane.getColumnConstraints().get(1).setPercentWidth(0.0);
+        // this.gridPane.getColumnConstraints().get(0).setPercentWidth(100.0);
+        // this.gridPane.getColumnConstraints().get(1).setPercentWidth(0.0);
+
+        this.taskInformationView.getChildren().clear();
+        this.taskInformationView.setAlignment(Pos.CENTER);
+        this.taskInformationView.setStyle("-fx-border-color: #3f2a03; -fx-border-radius: 10; -fx-border-width: 2; "
+                + "-fx-background-radius: 10; -fx-background-color: #FCF6DB; -fx-border-insets: 5; "
+                + "-fx-background-insets: 5");
+        this.taskInformationView.setPadding(
+                new Insets(5, 5, 5, 5)
+        );
+
+        this.taskInformationView.getChildren().add(
+                new Label("Select a Task...")
+        );
+
     }
 
     private Label getDescriptionField(Task task) {
@@ -162,22 +150,22 @@ public class TaskListPanel extends UiPart<Region> {
         Label priorityLabel = new Label(priority.toString());
 
         switch (priority) {
-            case LOW:
-                priorityLabel.setStyle("-fx-text-fill: black; -fx-background-color: #7ED957; -fx-padding: 1 4 1 4; "
-                        + "-fx-border-radius: 2; -fx-background-radius: 0 10 0 10; -fx-font-size: 11");
-                break;
-            case MEDIUM:
-                priorityLabel.setStyle("-fx-text-fill: black; -fx-background-color: #FFDE59; -fx-padding: 1 4 1 4; "
-                        + "-fx-border-radius: 2; -fx-background-radius: 0 10 0 10; -fx-font-size: 11");
-                break;
-            case HIGH:
-                priorityLabel.setStyle("-fx-text-fill: black; -fx-background-color: #FF443A; -fx-padding: 1 4 1 4; "
-                        + "-fx-border-radius: 2; -fx-background-radius: 0 10 0 10; -fx-font-size: 11");
-                break;
-            default:
-                priorityLabel.setStyle("-fx-text-fill: black; -fx-background-color: #c2c1c1; -fx-padding: 1 4 1 4; "
-                        + "-fx-border-radius: 2; -fx-background-radius: 0 10 0 10; -fx-font-size: 11");
-                break;
+        case LOW:
+            priorityLabel.setStyle("-fx-text-fill: black; -fx-background-color: #7ED957; -fx-padding: 1 4 1 4; "
+                    + "-fx-border-radius: 2; -fx-background-radius: 0 10 0 10; -fx-font-size: 11");
+            break;
+        case MEDIUM:
+            priorityLabel.setStyle("-fx-text-fill: black; -fx-background-color: #FFDE59; -fx-padding: 1 4 1 4; "
+                    + "-fx-border-radius: 2; -fx-background-radius: 0 10 0 10; -fx-font-size: 11");
+            break;
+        case HIGH:
+            priorityLabel.setStyle("-fx-text-fill: black; -fx-background-color: #FF443A; -fx-padding: 1 4 1 4; "
+                    + "-fx-border-radius: 2; -fx-background-radius: 0 10 0 10; -fx-font-size: 11");
+            break;
+        default:
+            priorityLabel.setStyle("-fx-text-fill: black; -fx-background-color: #c2c1c1; -fx-padding: 1 4 1 4; "
+                    + "-fx-border-radius: 2; -fx-background-radius: 0 10 0 10; -fx-font-size: 11");
+            break;
         }
 
         box.getChildren().addAll(title, priorityLabel);
