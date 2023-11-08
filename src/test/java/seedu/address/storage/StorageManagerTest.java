bb7c0e7d6b24f9aea8bfc8900ab9b5327f8aa51b
Sebastian Tay <taysebastian95@gmail.com>
2023-11-08 20:13:39 +0800

    Fix checkstyle

--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -48,7 +48,7 @@ public class StorageManagerTest {
     }
 
     @Test
-    public void TaskWiseReadSave() throws Exception {
+    public void taskWiseReadSave() throws Exception {
         /*
          * Note: This is an integration test that verifies the StorageManager is properly wired to the
          * {@link JsonTaskWiseStorage} class.
